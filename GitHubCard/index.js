19d057988fae000f6a2c87cb0861725c355d7159
CloudyNC <geoffreykellync@gmail.com>
2022-02-10 17:49:48 -0500

    init commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,16 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/GeoffKellyNC')
+  .then(response => {
+    entryPoint.appendChild(githubCard(response.data))
+  })
+  .catch(err => console.log(err));
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -34,6 +42,7 @@ const followersArray = [];
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
 
+
     <div class="card">
       <img src={image url of user} />
       <div class="card-info">
@@ -50,6 +59,54 @@ const followersArray = [];
     </div>
 */
 
+const entryPoint = document.querySelector(".cards")
+
+function githubCard(gitObj){
+  const gitCard = document.createElement('div');
+  const gitImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const realName = document.createElement('h3');
+  const userName = document.createElement('p')
+  const userLocation = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  gitCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  realName.classList.add('name');
+  userName.classList.add('username');
+
+  gitImg.src = gitObj.avatar_url;
+  gitImg.alt = 'Github Profile Image.'
+  realName.textContent = gitObj.name;
+  userName.textContent = gitObj.username;
+  userLocation.textContent = gitObj.location;
+  profile.textContent = 'Profile: '
+  profileLink.textContent = gitObj.html_url;
+  followers.textContent = `Followers: ${gitObj.followers}`;
+  following.textContent = `Following: ${gitObj.following}`;
+  bio.textContent = gitObj.bio;
+
+  gitCard.appendChild(gitImg);
+  gitCard.appendChild(cardInfo);
+  cardInfo.appendChild(realName);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+
+  return gitCard;
+
+}
+
+
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -57,4 +114,6 @@ const followersArray = [];
     justsml
     luishrd
     bigknell
+    crharding
 */
+
